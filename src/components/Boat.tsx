e50ab9395dc722cb12b08b3444ac071211912da9
Sundrun <not@an.email>
2025-09-20 11:04:00 +0200

    svg

--- src/components/Boat.tsx
+++ src/components/Boat.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ReactComponent as BoatIcon } from '../assets/boat.svg';
 
 interface BoatProps {
   x: number;
@@ -16,16 +17,7 @@ const Boat: React.FC<BoatProps> = ({ x, y, rotation, onDragStart }) => {
       style={{ cursor: 'grab' }}
       className="boat-handle"
     >
-      <path
-        d="M -6 2 L 6 2 L 4 -2 L -4 -2 Z"  // Simple boat shape
-        fill="blue"
-        stroke="none"
-      />
-      <path
-        d="M -1 -2 L -1 -5 L 1 -5 L 1 -2"  // Small cabin/mast
-        fill="blue"
-        stroke="none"
-      />
+      <BoatIcon />
     </g>
   );
 };
